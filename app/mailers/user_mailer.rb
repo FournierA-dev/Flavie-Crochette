70aa74be807adde309f966871d8df3504a118fda
FournierA-dev <yop.fou@laposte.net>
2019-09-23 17:03:50 +0200

    Unabling mail sending for heroku seed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,17 +5,17 @@ class UserMailer < ApplicationMailer
     @user = user
     @url  = 'https://flavie-crochette.herokuapp.com/users/sign_in' 
     @recipient = user
-    mail(to: @recipient, subject: 'Bienvenue chez Flavie Crochette !')
+   # mail(to: @recipient, subject: 'Bienvenue chez Flavie Crochette !')
     @recipient = $ADMIN
-    mail(to: @recipient, subject: 'Nouvel inscription sur le site crochette') 
+   # mail(to: @recipient, subject: 'Nouvel inscription sur le site crochette') 
   end
 
   def new_attendance_email(attendance)
     @attendance = attendance 
     @recipient = attendance.attendee.email
-    mail(to: @recipient, subject: "Confirmation d'inscription à un événement chez Flavie Crochette")
+   # mail(to: @recipient, subject: "Confirmation d'inscription à un événement chez Flavie Crochette")
     @recipient = $ADMIN
-    mail(to: @recipient, subject: "Nouvelle participation d'un inscrit à un événement") 
+   # mail(to: @recipient, subject: "Nouvelle participation d'un inscrit à un événement") 
   end
 
 
